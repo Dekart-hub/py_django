f326b87a553035cf04ae853c366c457a72808948
Dekart <dekarter@protonmail.com>
2023-02-28 22:34:05 +0300

    + db scheme; models; postgreSQL

--- cafeteria/settings.py
+++ cafeteria/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "cafeteria.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgre",
+        "USER": "potgre",
+        "PASSWORD": "123",
+        "HOST": "localhost",
+        "PORT": 5432
     }
 }
 
--- web/models.py
+++ web/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    cal = models.IntegerField()
+    weight = models.IntegerField()
+    price = models.IntegerField()
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=256)
+    type = models.CharField(max_length=256)
+    weight = models.IntegerField()
+    products = models.ManyToManyField(Product)
+
+
+class Cook(models.Model):
+    count = models.IntegerField()
+    id_dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    prep_date = models.DateTimeField()
+
+
